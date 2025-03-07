69088a9c00d8116c5175c879f422aa750b64551f
Ehsan <ehsansafavi1384@gmail.com>
2025-03-07 23:19:31 +0330

    Update numbers.java

--- numbers.java
+++ numbers.java
@@ -1,9 +1,19 @@
 public class numbers {
-    public static void main()
+    public static void main(String[] args)
      {
-        for( int i = 1 ;i <= 100 ; i++)
+        for(int i=2; i<=100; i++)
         {
-            System.out.println(i);
+            boolean number_is_aval = true ;
+            for(int j=2; j<i; j++)
+            {
+                if(i%j == 0)
+                {
+                    number_is_aval = false ;
+                }
+            }
+            if(number_is_aval == true){
+                System.out.println(i+" aval ast");
+            }
         }
     }
 }
